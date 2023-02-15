d304667c5a7796d1a1853e860a7527490e71e736
Nguyen The Hung <the_hung_nguyen@yahoo.com>
2023-02-14 19:24:45 -0500

    Indentation formatting

--- ProjectLWC/force-app/main/default/lwc/data/data.js
+++ ProjectLWC/force-app/main/default/lwc/data/data.js
@@ -1,46 +1,47 @@
 export const bikes = [
-    {"apiName":"Product__c",
-     "childRelationships":{},
-     "fields":{
-        "Category__c":{
-            "displayValue":"Mountain",
-            "value":"Mountain"},
-        "CreatedDate":{
-            "displayValue":null,
-            "value":"2018-10-09T03:29:52.000Z"},
-        "Description__c":{
-            "displayValue":null,
-            "value":"A durable e-bike with great looks."},
-        "Id":{
-            "displayValue":null,
-            "value":"a0256000001F1arAAC"},
-        "LastModifiedDate":{
-            "displayValue":null,
-            "value":"2018-10-12T02:57:48.000Z"},
-        "Level__c":{
-            "displayValue":"Racer",
-            "value":"Racer"},
-        "MSRP__c":{
-            "displayValue":"$7,800",
-            "value":7800},
-        "Material__c":{
-            "displayValue":"Carbon",
-            "value":"Carbon"},
-        "Name":{
-            "displayValue":null,
-            "value":"DYNAMO X1"},
-        "Picture_URL__c":{
-            "displayValue":null,
-            "value":"https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/dynamox1.jpg"},
-        "SystemModstamp":{
-            "displayValue":null,
-            "value":"2018-10-12T02:57:48.000Z"}
+    {   
+        "apiName":"Product__c",
+        "childRelationships":{},
+        "fields":{
+            "Category__c":{
+                "displayValue":"Mountain",
+                "value":"Mountain"},
+            "CreatedDate":{
+                "displayValue":null,
+                "value":"2018-10-09T03:29:52.000Z"},
+            "Description__c":{
+                "displayValue":null,
+                "value":"A durable e-bike with great looks."},
+            "Id":{
+                "displayValue":null,
+                "value":"a0256000001F1arAAC"},
+            "LastModifiedDate":{
+                "displayValue":null,
+                "value":"2018-10-12T02:57:48.000Z"},
+            "Level__c":{
+                "displayValue":"Racer",
+                "value":"Racer"},
+            "MSRP__c":{
+                "displayValue":"$7,800",
+                "value":7800},
+            "Material__c":{
+                "displayValue":"Carbon",
+                "value":"Carbon"},
+            "Name":{
+                "displayValue":null,
+                "value":"DYNAMO X1"},
+            "Picture_URL__c":{
+                "displayValue":null,
+                "value":"https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/dynamox1.jpg"},
+            "SystemModstamp":{
+                "displayValue":null,
+                "value":"2018-10-12T02:57:48.000Z"}
         },
-    "id":"a0256000001F1arAAC",
-    "lastModifiedById":null,
-    "lastModifiedDate":"2018-10-12T02:57:48.000Z",
-    "recordTypeInfo":null,
-    "systemModstamp":"2018-10-12T02:57:48.000Z"
+        "id":"a0256000001F1arAAC",
+        "lastModifiedById":null,
+        "lastModifiedDate":"2018-10-12T02:57:48.000Z",
+        "recordTypeInfo":null,
+        "systemModstamp":"2018-10-12T02:57:48.000Z"
     },
     {"apiName":"Product__c","childRelationships":{},"fields":{"Category__c":{"displayValue":"Mountain","value":"Mountain"},"CreatedDate":{"displayValue":null,"value":"2018-10-09T03:29:52.000Z"},"Description__c":{"displayValue":null,"value":"A durable e-bike with great looks."},"Id":{"displayValue":null,"value":"a0256000001F1atAAC"},"LastModifiedDate":{"displayValue":null,"value":"2018-10-10T17:26:47.000Z"},"Level__c":{"displayValue":"Racer","value":"Racer"},"MSRP__c":{"displayValue":"$6,802","value":6802},"Material__c":{"displayValue":"Aluminum","value":"Aluminum"},"Name":{"displayValue":null,"value":"DYNAMO X2"},"Picture_URL__c":{"displayValue":null,"value":"https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/dynamox2.jpg"},"SystemModstamp":{"displayValue":null,"value":"2018-10-10T17:26:47.000Z"}},"id":"a0256000001F1atAAC","lastModifiedById":null,"lastModifiedDate":"2018-10-10T17:26:47.000Z","recordTypeInfo":null,"systemModstamp":"2018-10-10T17:26:47.000Z"},
     {"apiName":"Product__c","childRelationships":{},"fields":{"Category__c":{"displayValue":"Mountain","value":"Mountain"},"CreatedDate":{"displayValue":null,"value":"2018-10-09T03:29:52.000Z"},"Description__c":{"displayValue":null,"value":"A durable e-bike with great looks."},"Id":{"displayValue":null,"value":"a0256000001F1auAAC"},"LastModifiedDate":{"displayValue":null,"value":"2018-10-09T04:37:56.000Z"},"Level__c":{"displayValue":"Enthusiast","value":"Enthusiast"},"MSRP__c":{"displayValue":"$5,601","value":5601},"Material__c":{"displayValue":"Aluminum","value":"Aluminum"},"Name":{"displayValue":null,"value":"DYNAMO X3"},"Picture_URL__c":{"displayValue":null,"value":"https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/dynamox3.jpg"},"SystemModstamp":{"displayValue":null,"value":"2018-10-09T04:37:56.000Z"}},"id":"a0256000001F1auAAC","lastModifiedById":null,"lastModifiedDate":"2018-10-09T04:37:56.000Z","recordTypeInfo":null,"systemModstamp":"2018-10-09T04:37:56.000Z"},
